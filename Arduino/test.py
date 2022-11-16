194920d0d3d3b4da7ccf12b4b10c612266f65fea
ViktoryaK <96166901+ViktoryaK@users.noreply.github.com>
2022-11-16 23:13:37 +0200

    Add files via upload

---
+++ Arduino/test.py
@@ -0,0 +1,23 @@
+## Importing Necessary Modules
+import requests  # to get image from the web
+import shutil  # to save it locally
+
+## Set up the image URL and filename
+image_url = "http://192.168.182.237/photo.jpg"
+filename = image_url.split("/")[-1]
+
+# Open the url image, set stream to True, this will return the stream content.
+r = requests.get(image_url, stream=True)
+
+# Check if the image was retrieved successfully
+if r.status_code == 200:
+    # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+    r.raw.decode_content = True
+
+    # Open a local file with wb ( write binary ) permission.
+    with open(filename, 'wb') as f:
+        shutil.copyfileobj(r.raw, f)
+
+    print('Image sucessfully Downloaded: ', filename)
+else:
+    print('Image Couldn\'t be retreived')
