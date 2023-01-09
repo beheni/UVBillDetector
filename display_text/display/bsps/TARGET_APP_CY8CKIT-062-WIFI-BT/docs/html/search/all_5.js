5313dce4a3255dba0dfd7f388b558dfb1880e2bc
Olli <91615532+linndfors@users.noreply.github.com>
2023-01-09 22:52:50 +0200

    Delete display_text directory

--- display_text/display/bsps/TARGET_APP_CY8CKIT-062-WIFI-BT/docs/html/search/all_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['j2_20header_20pins_0',['J2 Header Pins',['../group__group__bsp__pins__j2.html',1,'']]]
-];
--- display_text/display/bsps/TARGET_APP_CY8CKIT-062-WIFI-BT/docs/html/search/groups_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['arduino_20header_20pins_0',['Arduino Header Pins',['../group__group__bsp__pins__arduino.html',1,'']]]
-];
