6c092eadad984d2cb942ee9e8b14c157a552cc52
ViktoryaK <96166901+ViktoryaK@users.noreply.github.com>
2023-01-18 09:52:53 +0200

    nn

--- Arduino/main.py
+++ Arduino/main.py
@@ -1,19 +1,18 @@
-# import serial
+import serial
 from Arduino import test_for_one
 from test import prepare_and_test
-# ser = serial.Serial(
-#     port='COM9',
-#     baudrate=115200,
-#     parity=serial.PARITY_NONE,
-#     stopbits=serial.STOPBITS_ONE,
-#     bytesize=serial.EIGHTBITS,
-#     timeout=0)
 
+ser = serial.Serial(
+    port='COM9',
+    baudrate=115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=0)
 
-prepare_and_test()
-test_for_one.predict("saved-photo.jpg")
+while True:
+    prepare_and_test()
+    response = test_for_one.predict("saved-photo.jpg")
+    ser.write(bytes(response))
 
-# ser.write(bytes(response))
-#
-#
-# ser.close()
+ser.close()
